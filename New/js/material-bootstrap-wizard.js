ee46e775c505fdf395451156b9d78e7a822b61ef
AleksaDjrodjic <33227154+AleksaDjrodjic@users.noreply.github.com>
2018-12-28 17:13:50 +0100

    Another Update ....

--- New/js/material-bootstrap-wizard.js
+++ New/js/material-bootstrap-wizard.js
@@ -29,11 +29,7 @@ $(document).ready(function(){
     // Code for the Validator
     var $validator = $('.wizard-card form').validate({
 		  rules: {
-		    firstname: {
-		      required: true,
-		      minlength: 3
-		    },
-		    lastname: {
+		    name: {
 		      required: true,
 		      minlength: 3
 		    },
